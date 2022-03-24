6fa5fbaaa06ff2159538ee7d5f2514a4fa9b3579
lucasceifa <101906493+lucasceifa@users.noreply.github.com>
2022-03-24 15:38:43 -0300

    Add files via upload

--- opa.py
+++ opa.py
@@ -6,16 +6,19 @@ while loop == 1: #comeco loop
     aa=0
     bb=len(num)-1
     print('''Tipo do seu numero: 
-    [ 1 ] BINARIO;
-    [ 2 ] OCTAL;
-    [ 3 ] HEXADECIMAL;
-    [ 4 ] DECIMAL.''')
+    [ 1 ] BINARIO - DECIMAL;
+    [ 2 ] OCTAL - DECIMAL;
+    [ 3 ] HEXADECIMAL - DECIMAL;
+    [ 4 ] DECIMAL - BINÁRIO.
+    [ 5 ] DECIMAL - OCTADECIMAL;
+    [ 6 ] DECIMAL - HEXADECIMAL;
+   ''')
     opcao = int(input("SUA OPCAO: "))
 
     if opcao == 1:
         i=0
-        aa=0
-        bb=len(num)-1
+        bb=0
+        aa=len(num)-1
         if num.isdigit():
             while i < len(num):
                 numa = int(num[i]) * 2 ** bb
@@ -24,13 +27,13 @@ while loop == 1: #comeco loop
                 i += 1
             else:
                 print(aa)
-                fim = str(input('Deseja continuar? [s] ou [n]'))
+                fim = str(input('Deseja continuar? [s] ou [n] '))
                 if fim == 'n':
                     break
                 else:
                     loop == 1
         else:
-            print('Digite um número válido.')
+            print('Digite um número válido. ')
 
     if opcao == 2:
         i=0
@@ -44,13 +47,43 @@ while loop == 1: #comeco loop
                 i += 1
             else:
                 print(aa)
-                fim = str(input('Deseja continuar? [s] ou [n]'))
+                fim = str(input('Deseja continuar? [s] ou [n] '))
                 if fim == 'n':
                     break
                 else:
                     loop == 1
         else:
-            print('Digite um número válido.')
+            print('Digite um número válido. ')
+    if opcao == 4:
+        resto = 0
+        new = int(num)
+        rest = []
+        while new > 0:
+            resto = new % 2
+            new = new // 2
+            rest.append(resto)
+        else:
+            print(rest[::-1])
+            fim = str(input('Deseja continuar? [s] ou [n] '))
+            if fim == 'n':
+                break
+            else:
+                loop == 1
+    if opcao == 5:
+        resto = 0
+        new = int(num)
+        rest = []
+        while new > 0:
+            resto = new % 8
+            new = new // 8
+            rest.append(resto)
+        else:
+            print(rest[::-1])
+            fim = str(input('Deseja continuar? [s] ou [n] '))
+            if fim == 'n':
+                break
+            else:
+                loop == 1
 
 
 
